a4f3a81080827cd53abc85e3cab54e06c913b718
francisco.piccirilli@gmail.com <francisco.piccirilli@gmail.com>
2024-05-25 09:52:27 +0200

    Actualizar Image.jsx

---
+++ src/components/Image.jsx
@@ -0,0 +1,9 @@
+// src/components/Image.jsx
+
+import styles from './Image.module.css';
+
+const Image = () => {
+  return <img className={styles.image} src="https://reactjs.org/logo-og.png" alt="ReactJS" />;
+};
+
+export default Image;
