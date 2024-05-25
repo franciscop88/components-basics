0100572d0751988b5a3ec90b861ff6f1a6e8d217
francisco.piccirilli@gmail.com <francisco.piccirilli@gmail.com>
2024-05-25 10:35:06 +0200

    Actualizar estilos de imagen y subtítulo

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,8 @@
+// src/components/Paragraph.jsx
+import styles from './Paragraph.module.css';
+
+const Paragraph = () => {
+  return <p className={styles.paragraph}>Este es un párrafo de ejemplo con ReactJS.</p>;
+};
+
+export default Paragraph;
---
+++ src/components/SubTitle.jsx
@@ -0,0 +1,7 @@
+import styles from './SubTitle.module.css';
+
+const SubTitle = () => {
+  return <h2 className={styles.subtitle}>This is a example components with ReactJS</h2>;
+};
+
+export default SubTitle;
